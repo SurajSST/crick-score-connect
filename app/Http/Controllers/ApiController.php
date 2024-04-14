2821538ce651adcae27352e71ade4da14ad7b3a7
SurajSST <senthakuris@gmail.com>
2024-04-14 22:54:52 +0545

    fixed typo on bowling

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -139,7 +139,7 @@ class ApiController extends Controller
                     'strikeRate' => '0', // Calculate strike rate if needed
                     'maidens' => (string)$totalMaidens,
                     'wickets' => (string)$totalWickets,
-                    'bowling' => (string)$bBowling,
+                    'bbowling' => (string)$bBowling,
                     'ecoRate' => (string)round($bowlingEconomyRate, 2),
                 ],
             ], 200);
