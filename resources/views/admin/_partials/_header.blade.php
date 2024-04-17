87e0fc8b6852e90822231c3b90c385d5c11448fe
SurajSST <senthakuris@gmail.com>
2024-04-17 22:12:18 +0545

    admin

--- resources/views/admin/_partials/_header.blade.php
+++ resources/views/admin/_partials/_header.blade.php
@@ -2,12 +2,24 @@
     <div class="header-content">
         <nav class="navbar navbar-expand">
             <div class="collapse navbar-collapse justify-content-between">
-                <a href="{{ route('storage.link') }}" class="btn btn-primary">Storage Link</a>
+                <div class="header-left">
+                    <div class="dashboard_bar">
+                        <div class="input-group search-area d-lg-inline-flex d-none">
+                            <input type="text" class="form-control" placeholder="Search here...">
+                            <div class="input-group-append">
+                                <span class="input-group-text"><a href="javascript:void(0)"><i class="flaticon-381-search-2"></i></a></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <ul class="navbar-nav header-right">
                     <li class="nav-item dropdown header-profile">
                         <a class="nav-link" href="javascript:void(0)" role="button" data-toggle="dropdown">
-                            <!-- <img src="images/profile/17.jpg" width="20" alt="" /> -->
-                            {{Auth::user()->name}}
+                            <div class="header-info">
+                                <span class="text-black">Hello, <strong>{{Auth::User()->name}}</strong></span>
+                                <p class="fs-12 mb-0">{{Auth::User()->role}}</p>
+                            </div>
+                            <img src="{{ asset('storage/' . Auth::user()->profile_photo_path) }}" width="20" alt="" />
                         </a>
                         <div class="dropdown-menu dropdown-menu-right">
                             <a href="{{ route('profile.show') }}" class="dropdown-item ai-icon">
@@ -15,18 +27,18 @@
                                     <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                                     <circle cx="12" cy="7" r="4"></circle>
                                 </svg>
-                                <span class="ml-2">Profile</span>
+                                <span class="ml-2">Profile </span>
                             </a>
-                            <form method="POST" action="{{ route('logout') }}" class="dropdown-item ai-icon">
+                            <a href="#" onclick="document.getElementById('logout-form').submit()" class="dropdown-item ai-icon">
+                                <svg id="icon-logout" xmlns="http://www.w3.org/2000/svg" class="text-danger" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                    <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+                                    <polyline points="16 17 21 12 16 7"></polyline>
+                                    <line x1="21" y1="12" x2="9" y2="12"></line>
+                                </svg>
+                                <span class="ml-2">Logout</span>
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                 @csrf
-                                <button type="submit" style="background: none; border: none;">
-                                    <svg id="icon-logout" xmlns="http://www.w3.org/2000/svg" class="text-danger" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                        <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
-                                        <polyline points="16 17 21 12 16 7"></polyline>
-                                        <line x1="21" y1="12" x2="9" y2="12"></line>
-                                    </svg>
-                                    <span class="ml-2">Logout</span>
-                                </button>
                             </form>
 
                         </div>
